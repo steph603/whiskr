46510497a30d8f6bfbf0119b6a2505e8cb5df8f4
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-10 14:03:33 +1100

    Cancan working for Services - need to modify views and apply to other models

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,5 @@
 class ServicesController < ApplicationController
+  load_and_authorize_resource
   before_action :set_service, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :my_services]
   # GET /services
