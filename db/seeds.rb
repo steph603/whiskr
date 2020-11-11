1cc17ab441cc14f7cbeefdf1305fcb0f0a3ffdd4
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-11 13:43:17 +1100

    Faked some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    User.create(fname: Faker::Name.first_name, lname: Faker::Name.last_name, email: Faker::Internet.unique.email, password: 'password', is_nurse: true)
+    User.create(fname: Faker::Name.first_name, lname: Faker::Name.last_name, email: Faker::Internet.unique.email, password: 'password', is_nurse: false)
+end
+30.times do
+    Pet.create(name: Faker::Creature::Cat.name, user_id: rand(3..25))
+end
+
+30.times do
+    Service.create(name: Faker::Book.title, description: Faker::Quote.famous_last_words, price: rand(14..71), user_id: rand(5..15))
+end
