59c46f2dfc82d4a6ac1200ece31c332465906606
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-18 13:29:56 +1100

    Some validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,7 @@
 class Service < ApplicationRecord
     belongs_to :user
-    validates :price, presence: true, numericality: { greater_than: 0, less_than: 1000 }
-    validates :description, presence: true, length: { in: 10..120, message: "Must be between 10 and 120 characters"}
-    validates :name, presence: true, length: { in: 5..20, message: "Must be between 5 and 20 characters"}
+    validates :name, length: { in: 5..20, message: "Must be between 5 and 20 characters"}
+    validates :description, length: { in: 10..120, message: "Must be between 10 and 120 characters"}
+    validates :price, numericality: { greater_than: 0, less_than: 1000 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   has_many :bookings, :dependent => :destroy
   has_one_attached :picture, :dependent => :destroy
   has_one :address, :dependent => :destroy
+  
+  #I don't think a user will be able to not select a role - but just in case!!
+  validates :is_nurse, inclusion: { in: [ true, false ], message: "You must select a role" }
+  validates :fname, length: { in: 2..10, message: "must be between 2 and 10 characters"}
+  validates :lname, length: { in: 2..10, message: "must be between 2 and 10 characters"}
 end
